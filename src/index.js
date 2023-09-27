89517ebaeda7954f2be58b953fe73ae9edfce834
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-09-27 02:29:39 +0200

    ready- corrected mentor's recommendations

--- src/index.js
+++ src/index.js
@@ -3,34 +3,7 @@ import SlimSelect from 'slim-select';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const breedSelect = new SlimSelect({
-    select: '#breed-list',
-  });
-
-  // Pobieranie do listy SlimSelect
-  fetchBreeds()
-    .then((breeds) => {
-      const breedOptions = breeds.map((breed) => ({
-        value: breed.value,
-        text: breed.label,
-      }));
-
-      // zaktualizowanie listy SlimSelect
-      breedSelect.setData(breedOptions);
-    })
-    .catch((error) => {
-      console.error('An error occurred while retrieving information:', error);
-      Report.failure('Oops! Something went wrong!', 
-          'Try reloading the page! Click key F5',
-           'Close'
-        )
-        errorElement.classList.remove('hidden');
-        setLoadingState(false); // Wyłączam stan ładowania
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const breedSelect = document.querySelector('select.breed-select');
+  const breedSelectElement = document.querySelector('select.breed-select');
   const loader = document.querySelector('#loader');
   const catInfoDiv = document.querySelector('.cat-info');
   const errorElement = document.querySelector('#error'); // Zmieniony identyfikator
@@ -54,81 +27,103 @@ document.addEventListener('DOMContentLoaded', () => {
   // Funkcja do aktualizacji stanu ładowania
   function setLoadingState(isLoading) {
     if (isLoading) {
-      breedSelect.classList.add('hidden');
+      breedSelectElement.classList.add('hidden');
       loader.classList.remove('hidden');
       catInfoDiv.classList.add('hidden');
       errorElement.classList.add('hidden');
     } else {
-      breedSelect.classList.remove('hidden');
+      breedSelectElement.classList.remove('hidden');
       loader.classList.add('hidden');
       catInfoDiv.classList.remove('hidden'); // Pokazujemy div.cat-info
     }
   }
-  // Pobieranie i  wypełnianie danych select.breed-select
-  fetchBreeds()
-    .then((breeds) => {
+
+  // Pobieranie i wypełnianie danych select.breed-select
+  async function initialize() {
+    try {
+      setLoadingState(true); // Inicjalizacja stanu ładowania
+
+      // Pobranie listy ras
+      const breeds = await fetchBreeds();
+
+      // Uzupełnienie selekta SlimSelect
+      const breedOptions = breeds.map((breed) => ({
+        value: breed.value,
+        text: breed.label,
+      }));
+      breedOptions.unshift({
+        'text': 'Select a breed',
+        'value': 'empty',
+        'style': 'color: #a95fde'
+      });
+
+
+      const breedSelect = new SlimSelect({
+        select: '#breed-list',
+        data: breedOptions,
+      });
+
+      // Uzupełnienie selekta HTML
       breeds.forEach((breed) => {
         const option = document.createElement('option');
         option.value = breed.value;
         option.textContent = breed.label;
-        breedSelect.appendChild(option);
+        breedSelectElement.appendChild(option);
       });
-      setLoadingState(false); // Wyłącz stan ładowania
-    })
-    .catch((error) => {
+
+      setLoadingState(false); // Wyłączenie stanu ładowania
+    } catch (error) {
       console.error('An error occurred while retrieving information:', error);
-      // Notiflix
-      Report.failure('Oops! Something went wrong!', 
-                      'Try reloading the page! Click key F5',
-                       'Close'
-      )
+      Report.failure('Oops! Something went wrong!', 'Try reloading the page! Click key F5', 'Close');
       errorElement.classList.remove('hidden'); // Komunikat o błędzie
-      setLoadingState(false); // Wyłączeni stanu ładowania
-    });
+      setLoadingState(false); // Wyłączenie stanu ładowania
+    }
+  }
 
   // Inicjalizacja stanu ładowania
   setLoadingState(true);
 
   // Obsługa wyboru rasy
-  breedSelect.addEventListener('change', () => {
-    const selectedBreedId = breedSelect.value;
+  breedSelectElement.addEventListener('change', async () => {
+    const selectedBreedId = breedSelectElement.value;
 
-    // Aktualizacja stanu ładowania 
+    // Aktualizacja stanu ładowania
     setLoadingState(true);
 
     // Wyczyszczenie zawartości div.cat-info
     clearElement(catInfoDiv);
 
-    fetchCatByBreed(selectedBreedId)
-      .then((catData) => {
-        if (catData && catData.url) {
-          const catImage = document.createElement('img');
-          catImage.src = catData.url;
-          catImage.alt = 'Cat';
-          catInfoDiv.appendChild(catImage);
-        } else {
-          catInfoDiv.textContent = 'No information about this cat.';
-        }
+    try {
+      // Pobranie informacji o kocie wybranej rasy
+      const catData = await fetchCatByBreed(selectedBreedId);
 
-        // Wyświetlenie informacji o rasie z pogrubionym tekstem
-        if (catData && catData.breeds && catData.breeds.length > 0) {
-          const breed = catData.breeds[0]; // Pobierz obiekt z informacjami o rasie
-          addParagraph(catInfoDiv, `BREED: ${breed.name}`);
-          addParagraph(catInfoDiv, `DESCRIPTION: ${breed.description}`);
-          addParagraph(catInfoDiv, `TEMPERAMENT: ${breed.temperament}`);
-        }
+      if (catData && catData.url) {
+        const catImage = document.createElement('img');
+        catImage.src = catData.url;
+        catImage.alt = 'Cat';
+        catInfoDiv.appendChild(catImage);
+      } else {
+        catInfoDiv.textContent = 'Cat breeds are listed above.';
+      }
 
-        // Wyłączam stan ładowania
-        setLoadingState(false);
-      })
-      .catch((error) => {
-        console.error('An error occurred while retrieving information', error);
-        Report.failure('Oops! Something went wrong!', 
-          'Try reloading the page! Click key F5',
-           'Close'
-        )
-        errorElement.classList.remove('hidden'); // Pokaż komunikat o błędzie
-        setLoadingState(false); // Wyłączam stan ładowania
-      });
+      // Wyświetlenie informacji o rasie z pogrubionym tekstem
+      if (catData && catData.breeds && catData.breeds.length > 0) {
+        const breed = catData.breeds[0]; // Pobierz obiekt z informacjami o rasie
+        addParagraph(catInfoDiv, `BREED: ${breed.name}`);
+        addParagraph(catInfoDiv, `DESCRIPTION: ${breed.description}`);
+        addParagraph(catInfoDiv, `TEMPERAMENT: ${breed.temperament}`);
+      }
+
+      // Wyłączenie stanu ładowania
+      setLoadingState(false);
+    } catch (error) {
+      console.error('An error occurred while retrieving information', error);
+      Report.failure('Oops! Something went wrong!', 'Try reloading the page! Click key F5', 'Close');
+      errorElement.classList.remove('hidden'); // Pokaż komunikat o błędzie
+      setLoadingState(false); // Wyłączenie stanu ładowania
+    }
   });
+
+  // Inicjalizacja
+  initialize();
 });
